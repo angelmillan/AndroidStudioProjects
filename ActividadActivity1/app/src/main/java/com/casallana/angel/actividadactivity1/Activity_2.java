5124fa70d76ae40bf08e24dde7fb97eb3fb2640c
angelmillan <angelmillan@me.com>
2017-09-27 13:47:31 +0200

    Segundo Commit

---
+++ ActividadActivity1/app/src/main/java/com/casallana/angel/actividadactivity1/Activity_2.java
@@ -0,0 +1,14 @@
+package com.casallana.angel.actividadactivity1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Activity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a);
+
+    }
+    }
