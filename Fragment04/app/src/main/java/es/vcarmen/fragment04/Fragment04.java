7f4e52a17879a494cd98fac74698ac99fa688871
Angel Millán <angelmillan@me.com>
2017-10-11 13:50:06 +0200

    miercoles

---
+++ Fragment04/app/src/main/java/es/vcarmen/fragment04/Fragment04.java
@@ -0,0 +1,30 @@
+package es.vcarmen.fragment04;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Fragment04 extends FragmentActivity {
+    private Button btnMostrarDialogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment04);
+
+        btnMostrarDialogo = findViewById(R.id.boton);
+        btnMostrarDialogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragmento1 dialogoFrangment = Fragmento1.nuevaInstancia(" Nuevo fregmento diálogo ");
+                dialogoFrangment.show(getSupportFragmentManager(),"diálogo");
+            }
+        });
+    }
+
+    public void positivo() {
+
+    }
+}
---
+++ Fragment04/app/src/main/java/es/vcarmen/fragment04/Fragmento1.java
@@ -0,0 +1,41 @@
+package es.vcarmen.fragment04;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+
+public class Fragmento1 extends DialogFragment {
+
+    static Fragmento1 nuevaInstancia (String titulo) {
+        Fragmento1 frangment = new Fragmento1();
+        Bundle args = new Bundle();
+        args.putString("title", titulo);
+        frangment.setArguments(args);
+        return frangment;
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String titulo = getArguments().getString("title");
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(titulo)
+                .setIcon(R.mipmap.ic_launcher)
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ((Fragment04)getActivity()).positivo();
+                    }
+                })
+                .create();
+    }
+
+
+
+    }
+
+
+
