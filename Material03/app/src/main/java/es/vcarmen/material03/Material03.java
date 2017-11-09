7a6e4c7bb2d0e1c4540ec417e7c6597febd1b31c
Angel Millán <angelmillan@me.com>
2017-11-09 10:25:08 +0100

    terminada02 iniciada 03

---
+++ Material03/app/src/main/java/es/vcarmen/material03/Material03.java
@@ -0,0 +1,13 @@
+package es.vcarmen.material03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Material03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material03);
+    }
+}
